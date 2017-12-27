7c41475b57bed38e52694cb43bf465ea00c30a1e
Alter Lagos <alagos@users.noreply.github.com>
2017-12-27 17:27:00 -0300

    Just loading images when url is available

--- app/javascript/packs/components/article.jsx
+++ app/javascript/packs/components/article.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 
-export default ({name, url, description, image, datePublished}) => (
-  <div className="mdl-cell mdl-cell--4-col">
-    <div className="demo-card-wide mdl-card mdl-shadow--2dp">
-      <div className="mdl-card__title"
-           style={{'backgroundImage': `url('${(image || {}).contentUrl}')`}} >
+export default class Article extends React.Component {
+  render(){
+    const {name, url, description, image, datePublished} = this.props;
+    let imageStyle;
+    if (image) {
+      imageStyle = { 'backgroundImage': `url('${image.contentUrl}')` }
+    }
+    return(
+      <div className="mdl-cell mdl-cell--4-col">
+        <div className="demo-card-wide mdl-card mdl-shadow--2dp">
+          <div className="mdl-card__title" style={imageStyle} >
+          </div>
+          <h2 className="mdl-card__title-text">{name}</h2>
+          <div className="mdl-card__supporting-text">
+            {description}
+          </div>
+          <div className="mdl-card__actions mdl-card--border">
+            <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+               href={url} target='_blank'>
+              Go to article
+            </a>
+          </div>
+        </div>
       </div>
-      <h2 className="mdl-card__title-text">{name}</h2>
-      <div className="mdl-card__supporting-text">
-        {description}
-      </div>
-      <div className="mdl-card__actions mdl-card--border">
-        <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
-           href={url} target='_blank'>
-          Go to article
-        </a>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
